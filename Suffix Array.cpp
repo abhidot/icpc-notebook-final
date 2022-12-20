9eadd5e8dfa8e5bd8af9323d7021d5513cc60b4d
Yash Daga <60319283+yashdaga24@users.noreply.github.com>
2022-12-20 18:25:56 +0530

    Update Suffix Array.cpp

--- Suffix Array.cpp
+++ Suffix Array.cpp
@@ -13,14 +13,18 @@ vector<int> sort_cyclic_shifts(string const& s) {
     for (int i = 1; i < n; i++) {
         if (s[p[i]] != s[p[i-1]])
             classes++;
-        c[p[i]] = classes - 1;}
+        c[p[i]] = classes - 1;
+    }
     vector<int> pn(n), cn(n);
     for (int h = 0; (1 << h) < n; ++h) {
         for (int i = 0; i < n; i++) {
             pn[i] = p[i] - (1 << h);
             if (pn[i] < 0)
-                pn[i] += n;}
-        fill(cnt.begin(), cnt.begin() + classes, 0);
+                pn[i] += n;
+        }
+        rep(i,0,classes)
+        cnt[i]=0;
+        //fill(cnt.begin(), cnt.begin() + classes, 0);
         for (int i = 0; i < n; i++)
             cnt[c[pn[i]]]++;
         for (int i = 1; i < classes; i++)
@@ -34,11 +38,37 @@ vector<int> sort_cyclic_shifts(string const& s) {
             pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
             if (cur != prev)
                 ++classes;
-            cn[p[i]] = classes - 1;}
-        c.swap(cn);}
-    return p;}
+            cn[p[i]] = classes - 1;
+        }
+        c.swap(cn);
+    }
+    return p;
+}
 vector<int> suffix_array_construction(string s) {
     s += "$";
     vector<int> sorted_shifts = sort_cyclic_shifts(s);
     sorted_shifts.erase(sorted_shifts.begin());
-    return sorted_shifts;}
+    return sorted_shifts;
+}
+vector<int> lcp_construction(string const& s, vector<int> const& p) {
+    int n = s.size();
+    vector<int> rank(n, 0);
+    for (int i = 0; i < n; i++)
+        rank[p[i]] = i;
+
+    int k = 0;
+    vector<int> lcp(n-1, 0);
+    for (int i = 0; i < n; i++) {
+        if (rank[i] == n - 1) {
+            k = 0;
+            continue;
+        }
+        int j = p[rank[i] + 1];
+        while (i + k < n && j + k < n && s[i+k] == s[j+k])
+            k++;
+        lcp[rank[i]] = k;
+        if (k)
+            k--;
+    }
+    return lcp;
+}
